9142f8c25a0a8bbabeed59a779b84d48159662af
LuizFernando <barbosa.luiz@zup.com.br>
2025-02-28 09:10:47 -0300

    chore: primeiro commit

---
+++ src/test/java/com/exercicio/calculadora_teste/CalculadoraTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.calculadora_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
